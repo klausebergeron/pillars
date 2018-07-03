e270984fe3627da20012a5b96c45be8ae523e3c8
klausebergeron <klausebergeron@gmail.com>
2018-07-03 19:59:41 -0400

    toggler attempt 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,29 +7,43 @@ const Header = (props) => {
       <div className="bar">
         <div className="header__title">Pillars Funding</div>
         <Links />
+        <button onClick={Links.toggleLinks} className="hamburger"></button>
       </div>
-    )
+    );
 }
 
-const Links = () => (
-  <div className="links">
-    <div className="link">
-      <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    </div>
-    <div className="link">
-      <NavLink to="/about-us" activeClassName="is-active">About Us</NavLink>
-    </div>
-    <div className="link">
-      <NavLink to="/services" activeClassName="is-active">Services</NavLink>
-    </div>
-    <div className="link">
-      <NavLink to="/contact-us" activeClassName="is-active">Contact Us</NavLink>
-    </div>
-    <div className="link">
-      <NavLink to="/apply-now" activeClassName="is-active">Apply Now</NavLink>
-    </div>
-  </div>
-);
+class Links extends React.Component {
+  state = {
+    visible: true
+  };
+  toggleLinks = () => {
+    this.setState(prevState => ({
+        visible: !prevState.visible
+      }));
+    console.log(visible);
+  };
+    render(){
+      return (
+        <div className="links">
+          <div className="link">
+            <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+          </div>
+          <div className="link">
+            <NavLink to="/about-us" activeClassName="is-active">About Us</NavLink>
+          </div>
+          <div className="link">
+            <NavLink to="/services" activeClassName="is-active">Services</NavLink>
+          </div>
+          <div className="link">
+            <NavLink to="/contact-us" activeClassName="is-active">Contact Us</NavLink>
+          </div>
+          <div className="link">
+            <NavLink to="/apply-now" activeClassName="is-active">Apply Now</NavLink>
+          </div>
+        </div>
+      );
+    }
+}
 
 
 export default Header;
