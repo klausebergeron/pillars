79164d7c457b80cd13a82dbc895c9946bb2f73ca
klausebergeron <klausebergeron@gmail.com>
2018-07-03 19:03:28 -0400

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,35 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-  toggle() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
+const Header = (props) => {
+    console.log(props);
     return (
-      <div>
-        <Navbar>
-          <NavbarBrand href="/">Pillars Funding</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!this.state.collapsed} navbar>
-              <Nav navbar>
-                <NavItem className="link">
-                  <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-                </NavItem>
-                <NavItem className="link">
-                  <NavLink to="/about-us" activeClassName="is-active">About Us</NavLink>
-                </NavItem>
-                <NavItem className="link">
-                  <NavLink to="/services" activeClassName="is-active">Services</NavLink>
-                </NavItem>
-                <NavItem className="link">
-                  <NavLink to="/contact-us" activeClassName="is-active">Contact Us</NavLink>
-                </NavItem>
-                <NavItem className="link">
-                  <NavLink to="/apply-now" activeClassName="is-active">Apply Now</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-        </Navbar>
+      <div className="bar">
+        <div className="header__title">Pillars Funding</div>
+        <Links />
       </div>
     )
-  }
 }
+
+const Links = () => (
+  <div className="links">
+    <div className="link">
+      <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    </div>
+    <div className="link">
+      <NavLink to="/about-us" activeClassName="is-active">About Us</NavLink>
+    </div>
+    <div className="link">
+      <NavLink to="/services" activeClassName="is-active">Services</NavLink>
+    </div>
+    <div className="link">
+      <NavLink to="/contact-us" activeClassName="is-active">Contact Us</NavLink>
+    </div>
+    <div className="link">
+      <NavLink to="/apply-now" activeClassName="is-active">Apply Now</NavLink>
+    </div>
+  </div>
+);
+
+
+export default Header;
